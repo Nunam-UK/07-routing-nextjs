ed8dacaa10425c18ff79c0f99779e435f65943db
Nunam-UK <ivanolef.io@gmail.com>
2026-01-15 23:15:50 +0200

    fixed2 hw 07

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,22 @@ import Footer from '../components/Footer/Footer';
 import Providers from '../components/Providers/Providers';
 import './globals.css';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ 
+  children,
+  modal 
+}: { 
+  children: React.ReactNode;
+  modal: React.ReactNode; 
+}) {
   return (
     <html lang="en">
       <body>
         <Providers>
           <Header />
-          <main style={{ flex: 1 }}>{children}</main>
+          <main style={{ flex: 1 }}>
+            {children}
+            {modal} 
+          </main>
           <Footer />
         </Providers>
       </body>
